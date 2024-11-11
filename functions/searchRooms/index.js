73ab6a456c70418eac9b30010ba19f60132feeca
KT <thun.kristian@gmail.com>
2024-11-11 22:25:19 +0100

    Availability is set and meta data returned

--- functions/searchRooms/index.js
+++ functions/searchRooms/index.js
@@ -1,77 +1,81 @@
-const {marshall, unmarshall} = require('@aws-sdk/util-dynamodb');
+const { marshall, unmarshall } = require('@aws-sdk/util-dynamodb');
 const dynamoDb = require('../../config/dynamodb');
 const { QueryCommand } = require("@aws-sdk/client-dynamodb");
 
 
 
 module.exports.handler = async (event, context) => {
-    const {guests, startDate, endDate} = event.queryStringParameters || {};
+    const { guests, startDate, endDate } = event.queryStringParameters || {};
     if (!guests || !startDate || !endDate) {
         return {
-          statusCode: 400,
-          body: JSON.stringify({
-            error: 'Missing required query parameters: guests, startDate, and endDate are all required.',
-          }),
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'Missing required query parameters: guests, startDate, and endDate are all required.',
+            }),
         };
-      }
+    }
 
-    
-      const params = {
+
+    const params = {
         TableName: 'HotelTable',
         KeyConditionExpression: 'PK = :roomKey AND SK BETWEEN :startDate AND :endDate',
         ExpressionAttributeValues: {
-          ':roomKey': { S: 'ROOMS' },       // Sätt din partition key till "ROOM"
-          ':startDate': { S: startDate },  // Början av datumintervallet
-          ':endDate': { S: endDate }       // Slutet av datumintervallet
+            ':roomKey': { S: 'ROOMS' },       
+            ':startDate': { S: startDate },  
+            ':endDate': { S: endDate }       
         },
-      };
+    };
 
-      try {
+    try {
         const data = await dynamoDb.send(new QueryCommand(params));
         const rooms = data.Items.map(item => unmarshall(item));
-        const getLowestAvailablityOfRoomTypes = {}
+        // If the days isn't present in db, it is nothing booked that day. Add info for unbooked rooms
+        if (rooms.length === 0 ){
+            let newRooms = {
+                Rooms: [
+                    {"Type": "Single", "Beds": 1, "Price": 1000, "Availability": 2, "Bookings": [] },
+                    { "Type": "Double", "Beds": 2, "Price": 1500, "Availability": 15, "Bookings": [] },
+                    { "Type": "Suit", "Beds": 2, "Price": 1500, "Availability": 3, "Bookings": [] }
+                  ]
+            }
+            rooms.push(newRooms)
+        }
+     
+        const lowestAvailabilityByRoomType = {};
         rooms.forEach(day => {
             day.Rooms.forEach(roomType => {
-                let type = roomType.Type
-                if (getLowestAvailablityOfRoomTypes[type] === undefined || getLowestAvailablityOfRoomTypes[type] > roomType.Availability){
-                    getLowestAvailablityOfRoomTypes[type] = roomType.Availability
+                const type = roomType.Type;
+                if (!lowestAvailabilityByRoomType[type]) {
+                    lowestAvailabilityByRoomType[type] = {
+                        beds: roomType.Beds,
+                        pricePerNight: roomType.Price,
+                        availability: roomType.Availability 
+                    };
+                } else {
+                   
+                    if (lowestAvailabilityByRoomType[type].Availability > roomType.Availability) {
+                        lowestAvailabilityByRoomType[type].Availability = roomType.Availability;
+                    }
                 }
-            })
+            });
         });
-        
-        console.log(getLowestAvailablityOfRoomTypes)
-
-        // Nu vet vi vilka rum som det finns minst av i perioden typ kanske blir problem om det saknas datum
-        
-
-        // Collected all room lists
-
-        //check minimum 
-
-        //console.log(generateDateRange(startDate, endDate))
-        // Is our list complete
-
-        
-
-
 
         return {
-          statusCode: 200,
-          body: JSON.stringify({
-            message: 'Rooms retrieved successfully.',
-            rooms: rooms,
-          }),
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'Rooms retrieved successfully.',
+                rooms: lowestAvailabilityByRoomType,
+            }),
         };
-      } catch (error) {
+    } catch (error) {
         console.error('Error querying rooms:', error);
         return {
-          statusCode: 500,
-          body: JSON.stringify({ error: 'Could not retrieve rooms' }),
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Could not retrieve rooms' }),
         };
-      }
-    
+    }
 
-    console.log(guests, startDate, endDate)
+
 }
 
 function generateDateRange(startDate, endDate) {
